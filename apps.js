0e358661d6600b2a374c31b3c3d179e8d159c457
oye-banji <mohammedoyediran@gmail.com>
2024-08-14 19:34:47 -0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,72 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const body = document.querySelector('.container');
+const bg = document.querySelector('.weather');
+const updateUI = (data) => {
+    // DESTRUCTURE PROPERTIES
+    const {cityDets, weather} = data;
+    console.log(data)
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-5 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C / </span>
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;F</span>
+
+        </div>
+    `
+    // //UPDATING BACKGROUND UMAGES
+
+    // let bgImg = weather.IsDayTime ? 'img/dayweather.avif' : 'img/nightweather.avif';
+    // bg.innerHTML = `
+    //     <style>
+    //         body {
+    //         background-image: url(${bgImg});
+    //         background-repeat: no-repeat;
+    //         background-attachment: fixed;
+    //         background-size: 100% 100%;
+    //         }
+    //     </style>
+    // `
+    //UPDATE THE NIGHT/DAY & ICON IMAGES
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc)
+    body.setAttribute('src', timeSrc)
+
+
+    //REMOVE THE d-none CLASS IF PRESENT
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+}
+const updateCity = async (city) => {
+     const cityDets = await getCity(city);
+     const weather = await getWeather(cityDets.Key);
+
+     return { cityDets, weather };
+}
+cityForm.addEventListener('submit', e => {
+    // PREVENT DEFAULT ACTION
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset()
+
+    //UPDATE THE ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+})
+
---
+++ forecast.js
@@ -0,0 +1,27 @@
+const key = 'WBCDCSiGQvxL7ayobbOy7GE142EswDbv';
+
+// GET WEATHER INFO
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// GET CITY INFO
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
